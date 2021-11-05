13597f5cbb378349669fa6a815ed62f065442bfb
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-11-05 11:43:42 +0000

    initial commit

---
+++ controllers/controller.py
@@ -0,0 +1,3 @@
+from werkzeug.utils import redirect
+from app import app
+from flask import render_template, redirect
---
+++ models/game.py
@@ -0,0 +1,8 @@
+class Game:
+
+    def __init__(self):
+        self.check_result = {
+            "scissors": "paper",
+            "paper": "rock",
+            "rock": "scissors"
+        }
